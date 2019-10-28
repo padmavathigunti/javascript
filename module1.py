9a9ab8cdbf66d33b2cba061e446d76b34186e2e4
guntipadmavati <padmavathigunti422@gmail.com>
2019-10-28 22:58:06 +0530

    java script

---
+++ module1.py
@@ -0,0 +1,11 @@
+a=45
+b=65
+
+def add(c,d):
+	return c+d
+
+def sub(c,d):
+	return c-d
+
+def mul(c,d):
+	return c*d
---
+++ practice1.js
@@ -0,0 +1,43 @@
+<html>
+	/*<!--<script>
+	var i=1,j;
+	for(j=1;j<=7;j++){
+		for(i=1;i<=j;i++){
+		if(i%2==0){
+		document.write("1");
+		}
+		else{
+		console.log("0");
+		}
+		document.write("<br>");
+	}
+	}
+	
+	</script>-->
+	<!--<body>
+	<h1>hi</h1>
+	<button type="button" onclick="document.getElementById('d1').innerHTML=Date()">click me</button>
+	<p id ="d1"></p>
+	</body>-->
+	<!--<body>
+
+	<h2 onclick="abc()" id='demo'>my mom</h2>
+
+	<p id="demo" style="display:none">paddu paddu paddu</p>-->
+	<!--<h2 onclick="abc()" id='demo1'>my dad</h2>
+	<p id="demo1" style="display:none">sai sai sai sai</p>-->
+	<!--<script>
+	function abc(){
+	document.getElementById('demo').style.display="block";
+	//document.getElementById('demo1').style.display="block";
+	
+	}
+	</script>
+	</body>-->*/
+	<head>
+	function myFunction() {
+	document.getElementById("demo").innerHTML = "Paragraph changed.";
+	}
+	</head>
+	
+</html>
